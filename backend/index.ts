f0f6349de44f5ec27fb3c7abe46bc0c58175cb78
Shiv Bhadaniya <shivbhadaniya56@gmail.com>
2024-03-15 16:55:40 +0530

    initialization

---
+++ backend/index.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
